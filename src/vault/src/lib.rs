e8e876e9d8b9ef986b9a2a90e7e248764a65acc6
olsemeno <sendtosemenov@gmail.com>
2025-03-12 19:04:09 +0400

    Add icrc28

--- src/vault/src/lib.rs
+++ src/vault/src/lib.rs
@@ -147,6 +147,42 @@ fn pre_upgrade() {
     stable_save();
 }
 
+
+#[derive(CandidType, Deserialize, Eq, PartialEq, Debug)]
+pub struct SupportedStandard {
+    pub url: String,
+    pub name: String,
+}
+
+#[query]
+fn icrc10_supported_standards() -> Vec<SupportedStandard> {
+    vec![
+        SupportedStandard {
+            url: "https://github.com/dfinity/ICRC/blob/main/ICRCs/ICRC-10/ICRC-10.md".to_string(),
+            name: "ICRC-10".to_string(),
+        },
+        SupportedStandard {
+            url: "https://github.com/dfinity/wg-identity-authentication/blob/main/topics/icrc_28_trusted_origins.md".to_string(),
+            name: "ICRC-28".to_string(),
+        },
+    ]
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+pub struct Icrc28TrustedOriginsResponse {
+    pub trusted_origins: Vec<String>
+}
+
+// list every base URL that users will authenticate to your app from
+#[update]
+fn icrc28_trusted_origins() -> Icrc28TrustedOriginsResponse {
+    let trusted_origins = vec![
+        String::from("https://47r3x-paaaa-aaaao-qj6ha-cai.icp0.io"),
+    ];
+
+    Icrc28TrustedOriginsResponse { trusted_origins }
+}
+
 #[post_upgrade]
 pub async fn post_upgrade() {
     stable_restore()
