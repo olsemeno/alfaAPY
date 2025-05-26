d39bb87d7d4d979e30e875d1a0a6a08a66a2bcd2
v7m <vadymladimyrych@Vadyms-personal.local>
2025-05-26 03:56:23 +0200

    [AAP-25] Add admin to pool_stats

--- src/pool_stats/src/lib.rs
+++ src/pool_stats/src/lib.rs
@@ -1,5 +1,55 @@
+use candid::{CandidType, Deserialize, Principal};
+use serde::Serialize;
+use std::cell::RefCell;
+use ic_cdk::{call, id, trap, update};
+use ic_cdk::api::call::CallResult;
+
 pub mod pools;
 pub mod liquidity;
 pub mod service;
 pub mod repository;
 pub mod snapshots;
+
+#[derive(CandidType, Debug, Clone, Deserialize)]
+pub struct CanisterIdRequest {
+    #[serde(rename = "canister_id")]
+    pub canister_id: Principal,
+}
+
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
+struct Config {
+    operator: Option<Principal>,
+}
+
+thread_local! {
+    static CONFIG: RefCell<Config> = RefCell::new(
+        Config {
+            operator: None
+        }
+    );
+}
+
+/// Sets the admin principal.
+#[update]
+async fn set_admin(operator: Principal) {
+    let controllers = get_controllers().await;
+    if !controllers.contains(&ic_cdk::caller()) {
+        trap("Unauthorized: caller is not a controller");
+    }
+    CONFIG.with(|config| {
+        let mut config = config.borrow_mut();
+        config.operator = Some(operator);
+    });
+}
+
+async fn get_controllers() -> Vec<Principal> {
+    let res: CallResult<(ic_cdk::api::management_canister::main::CanisterStatusResponse,)> = call(
+        Principal::management_canister(),
+        "canister_status",
+        (CanisterIdRequest { canister_id: id() },),
+    )
+        .await;
+    res
+        .expect("Get controllers function exited unexpectedly: inter-canister call to management canister for canister_status returned an empty result.")
+        .0.settings.controllers
+}
